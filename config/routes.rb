4b1cf7625a50547f25ce0edb6fff8028388ff002
306-san <oh.taku528+git@gmail.com>
2015-08-19 23:40:00 +0900

    タスクの一覧画面作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'tasks/index'
+
+  get 'tasks/show'
+
+  get 'tasks/create'
+
   get 'home/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
